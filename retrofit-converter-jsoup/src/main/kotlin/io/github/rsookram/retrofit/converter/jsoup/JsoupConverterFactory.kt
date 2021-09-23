59f64a99b3d5c3981effedf16a780e0f54398b90
rsookram <rashadsookram@gmail.com>
2021-09-22 21:40:24 -0400

    Add doc comment

--- retrofit-converter-jsoup/src/main/kotlin/io/github/rsookram/retrofit/converter/jsoup/JsoupConverterFactory.kt
+++ retrofit-converter-jsoup/src/main/kotlin/io/github/rsookram/retrofit/converter/jsoup/JsoupConverterFactory.kt
@@ -6,6 +6,14 @@ import org.jsoup.nodes.Document
 import retrofit2.Converter
 import retrofit2.Retrofit
 
+/**
+ * A [Converter.Factory] which adds support for returning jsoup [Document]s in a Retrofit
+ * interface.
+ *
+ * @param charsetName character set of response body. Set to `null` to determine from `http-equiv`
+ * meta tag if present, falling back to UTF-8.
+ * @param baseUri The URL where the HTML was retrieved from, to resolve relative links against.
+ */
 class JsoupConverterFactory(
     private val charsetName: String? = "UTF-8",
     private val baseUri: String = "",
