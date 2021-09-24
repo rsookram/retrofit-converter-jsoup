acd7aeffb04a50172f46f0d509810d06a70aaf73
rsookram <rashadsookram@gmail.com>
2021-09-23 20:45:51 -0400

    Set up Dokka and maven publishing
    Based off
    https://github.com/Kotlin/dokka/blob/59e2e5f0c7275804d5fff9b85c92c1f7f4fb3387/examples/gradle/dokka-library-publishing-example/build.gradle.kts

--- retrofit-converter-jsoup/build.gradle.kts
+++ retrofit-converter-jsoup/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     kotlin("jvm")
+    id("org.jetbrains.dokka") version ("1.5.30")
     `java-library`
+    `maven-publish`
 }
 
 dependencies {
@@ -18,5 +20,32 @@ java {
 tasks.jar {
     manifest {
         attributes("Automatic-Module-Name" to "io.github.rsookram.retrofit.converter.jsoup")
+    }
+}
+
+val dokkaJavadocJar by tasks.register<Jar>("dokkaJavadocJar") {
+    dependsOn(tasks.dokkaJavadoc)
+    from(tasks.dokkaJavadoc.flatMap { it.outputDirectory })
+    archiveClassifier.set("javadoc")
+}
+
+val dokkaHtmlJar by tasks.register<Jar>("dokkaHtmlJar") {
+    dependsOn(tasks.dokkaHtml)
+    from(tasks.dokkaHtml.flatMap { it.outputDirectory })
+    archiveClassifier.set("html-doc")
+}
+
+publishing {
+    publications {
+        register<MavenPublication>("library") {
+            groupId = "io.github.rsookram"
+            artifactId = "retrofit-converter-jsoup"
+            version = "0.0.1"
+
+            from(components["java"])
+
+            artifact(dokkaJavadocJar)
+            artifact(dokkaHtmlJar)
+        }
     }
 }
